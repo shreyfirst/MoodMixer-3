a03b2f4232fbf4b7af9b999e46b5b3119cd1110e
shreyfirst <shreysgupta@gmail.com>
2018-01-07 14:13:49 -0800

    synced android gradle file

--- android/app/src/main/java/MoodMixer/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/MoodMixer/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package MoodMixer.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp0e746dc850724b25bd66bae63aadd684://localhost:80";
 
 }
